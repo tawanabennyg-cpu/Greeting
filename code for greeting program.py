361e7b797e11dba94e4e9007f9239243a5e6d597
tawanabennyg-cpu <tawanabennyg@gmail.com>
2025-10-27 03:44:10 +0200

    Update code for greeting program.py

--- code for greeting program.py
+++ code for greeting program.py
@@ -1,7 +1,8 @@
 # Program to greet the user
 def greet_user():
     name = input("What's your name? ")
-    print(f"Hello, {name}! Welcome . ")
+    print(f"Hello, {name}! Welcome  . ")
 # call the function
 
 greet_user() 
+
